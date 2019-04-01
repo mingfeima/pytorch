5fa44521739e65aad997940280420de3a1ce84db
Mingfei Ma <mingfei.ma@intel.com>
2019-04-01 14:49:07 +0800

    add mkldnn() under module.py

--- torch/backends/mkldnn/__init__.py
+++ torch/backends/mkldnn/__init__.py
@@ -1,5 +1,11 @@
 import torch
+import torch.nn as nn
 
+
+mkldnn_supported_op = {
+    nn.Conv2d,
+    nn.ConvTranspose2d,
+}
 
 def is_available():
     r"""Returns whether PyTorch is built with MKL-DNN support."""
