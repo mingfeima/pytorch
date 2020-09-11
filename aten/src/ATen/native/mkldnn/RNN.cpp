653c4b779f354a415b16e54fa663609c3085e6cb
mingfeima <mingfei.ma@intel.com>
2020-09-11 15:57:54 +0800

    lstm backward, to be finished

--- aten/src/ATen/native/mkldnn/RNN.cpp
+++ aten/src/ATen/native/mkldnn/RNN.cpp
@@ -29,12 +29,11 @@ std::tuple<Tensor, Tensor, Tensor> lstm_mkldnn_stub(
 }
 
 std::tuple<Tensor, Tensor, Tensor, Tensor> mkldnn_rnn(
-    const Tensor& input_r, TensorList weight, int64_t weight_stride0,
-    const Tensor& hx, const Tensor& cx,
-    int64_t fn_mode, int64_t fn_hidden_size,
-    int64_t fn_num_layers, bool batch_first, double fn_dropout,
-    bool fn_train, bool fn_bidirectional, IntArrayRef fn_batch_sizes,
-    const Tensor& fn_dropout_state) {
+    const Tensor& input_, TensorList weight, int64_t weight_stride0,
+    const Tensor& hx_, const Tensor& cx_,
+    int64_t mode, int64_t hidden_size,
+    int64_t num_layers, bool batch_first, double dropout_p,
+    bool train, bool bidirectional, IntArrayRef batch_sizes) {
   AT_ERROR("mkldnn_rnn: ATen not compiled with MKLDNN support");
 }
 
@@ -155,17 +154,17 @@ std::vector<int64_t> _output_size(const RNNParams& rnn) {
 //                           |   nt2   |
 //                           +---------+
 //
-Tensor _shuffle_weight(const Tensor& weight, int64_t fn_mode) {
+Tensor _shuffle_weight(const Tensor& weight, int64_t mode) {
   auto weight_t = weight.contiguous();
-  if (static_cast<ideep::rnn_kind>(fn_mode) == ideep::rnn_kind::GRU) {
+  if (static_cast<ideep::rnn_kind>(mode) == ideep::rnn_kind::GRU) {
     std::vector<Tensor> gates = weight_t.chunk(3, /*gates*/0);
     return at::cat({gates[1], gates[0], gates[2]}, /*gates*/0);
   }
   return weight_t;
 };
 
-Tensor _shuffle_bias(const Tensor& bias_ih, const Tensor& bias_hh, int64_t fn_mode) {
-  if (static_cast<ideep::rnn_kind>(fn_mode) == ideep::rnn_kind::GRU) {
+Tensor _shuffle_bias(const Tensor& bias_ih, const Tensor& bias_hh, int64_t mode) {
+  if (static_cast<ideep::rnn_kind>(mode) == ideep::rnn_kind::GRU) {
     std::vector<Tensor> b1 = bias_ih.chunk(3, /*output_channels*/0);
     std::vector<Tensor> b2 = bias_hh.chunk(3, /*output_channels*/0);
     return at::cat({b1[1] + b2[1], b1[0] + b2[0], b1[2], b2[2]}, /*output_channels*/0);
@@ -179,9 +178,14 @@ static inline ideep::tensor get_mkldnn_tensor(
   return {desc, tensor.data_ptr<float>()};
 }
 
+static inline ideep::tensor get_mkldnn_tensor(
+    void* data_ptr, const ideep::tensor::desc& desc) {
+  return {desc, data_ptr};
+}
+
 Tensor mkldnn_rnn_layer(Tensor& hy_, Tensor& cy_,
     const Tensor& input, TensorList weights,
-    const Tensor& hx_, const Tensor& cx_,
+    const Tensor& hx_, const Tensor& cx_, ideep::tensor& ws,
     bool reverse, const RNNParams& rnn) {
   TORCH_CHECK(weights.size() == 2 || weights.size() == 4);
 
@@ -195,29 +199,87 @@ Tensor mkldnn_rnn_layer(Tensor& hy_, Tensor& cy_,
                        : at::zeros({rnn.num_bias_gates * rnn.hidden_size}, weight_ih.options());
 
   // per layer input size
-  int64_t input_size = input.size(2);
-  auto x = get_mkldnn_tensor(input, rnn.src_layer_desc(input_size));
+  int64_t layer_input_size = input.size(2);
+  auto x = get_mkldnn_tensor(input, rnn.src_layer_desc(layer_input_size));
   auto hx = get_mkldnn_tensor(hx_, rnn.src_iter_desc());
-  auto w1 = get_mkldnn_tensor(weight_ih, rnn.weights_layer_desc(input_size));
+  auto w1 = get_mkldnn_tensor(weight_ih, rnn.weights_layer_desc(layer_input_size));
   auto w2 = get_mkldnn_tensor(weight_hh, rnn.weights_iter_desc());
   auto b = get_mkldnn_tensor(bias, rnn.bias_desc());
   auto y = get_mkldnn_tensor(output, rnn.dst_layer_desc());
   auto hy = get_mkldnn_tensor(hy_, rnn.dst_iter_desc());
 
   auto _rnn_kind = static_cast<ideep::rnn_kind>(rnn.mode);
-  if (_rnn_kind == ideep::rnn_kind::LSTM) {
-    auto cx = get_mkldnn_tensor(cx_, rnn.src_iter_desc());
-    auto cy = get_mkldnn_tensor(cy_, rnn.dst_iter_desc());
-    ideep::lstm_forward_inference::compute(x, hx, cx, w1, w2, b, y, hy, cy, reverse);
-  } else if (_rnn_kind == ideep::rnn_kind::GRU) {
-    ideep::gru_forward_inference::compute(x, hx, w1, w2, b, y, hy, reverse);
+  if (rnn.train) {
+    if (_rnn_kind == ideep::rnn_kind::LSTM) {
+      auto cx = get_mkldnn_tensor(cx_, rnn.src_iter_desc());
+      auto cy = get_mkldnn_tensor(cy_, rnn.dst_iter_desc());
+      ideep::lstm_forward::compute(x, hx, cx, w1, w2, b, y, hy, cy, ws, reverse);
+    }
+
   } else {
-    TORCH_CHECK(_rnn_kind == ideep::rnn_kind::RNN_RELU || _rnn_kind == ideep::rnn_kind::RNN_TANH,
-                "mkldnn_rnn: unsuppored rnn mode: ", rnn.mode);
-    ideep::rnn_forward_inference::compute(x, hx, w1, w2, b, y, hy, rnn.mode, reverse);
+    if (_rnn_kind == ideep::rnn_kind::LSTM) {
+      auto cx = get_mkldnn_tensor(cx_, rnn.src_iter_desc());
+      auto cy = get_mkldnn_tensor(cy_, rnn.dst_iter_desc());
+      ideep::lstm_forward::compute(x, hx, cx, w1, w2, b, y, hy, cy, reverse);
+    } else if (_rnn_kind == ideep::rnn_kind::GRU) {
+      ideep::gru_forward_inference::compute(x, hx, w1, w2, b, y, hy, reverse);
+    } else {
+      TORCH_CHECK(_rnn_kind == ideep::rnn_kind::RNN_RELU || _rnn_kind == ideep::rnn_kind::RNN_TANH,
+                  "mkldnn_rnn: unsuppored rnn mode: ", rnn.mode);
+      ideep::rnn_forward_inference::compute(x, hx, w1, w2, b, y, hy, rnn.mode, reverse);
+    }
   }
 
   return output;
+}
+
+// NB
+void prepare_workspace(
+    Tensor& workspace, std::vector<ideep::tensor>& mkldnn_workspace_arr,
+    const RNNParams& rnn) {
+  if (!rnn.train) { return; }
+  std::cout << "workspace.numel(): " << workspace.numel() << std::endl;
+
+  auto _rnn_kind = static_cast<ideep::rnn_kind>(rnn.mode);
+  auto num_layers = rnn.num_layers;
+  auto num_directions = rnn.num_directions;
+
+  std::vector<ideep::tensor::desc> workspace_desc_arr;
+  workspace_desc_arr.reserve(num_layers * num_directions);
+  for (int64_t layer = 0; layer < num_layers; layer++) {
+    for (int64_t direction = 0; direction < num_directions; direction++) {
+      int64_t layer_input_size = layer == 0 ? rnn.input_size
+                                            : rnn.hidden_size * num_directions;
+      auto reverse = (direction > 0);
+      if (_rnn_kind == ideep::rnn_kind::LSTM) {
+        auto desc = ideep::lstm_forward::get_workspace_desc(
+            rnn.src_layer_desc(layer_input_size), rnn.src_iter_desc(), rnn.src_iter_desc(),
+            rnn.weights_layer_desc(layer_input_size), rnn.weights_iter_desc(), rnn.bias_desc(),
+            rnn.dst_layer_desc(), rnn.dst_iter_desc(), rnn.dst_iter_desc(), reverse);
+        workspace_desc_arr.emplace_back(desc);
+      }
+    }
+  }
+
+  int64_t size_bytes = 0;
+  for (const auto& desc : workspace_desc_arr) {
+    size_bytes += desc.get_size();
+  }
+  if (workspace.numel() == 0) {
+    workspace.resize_({size_bytes});
+  } else {
+    TORCH_INTERNAL_ASSERT(workspace.numel() == size_bytes, "mkldnn_rnn: workspace size mismatch");
+  }
+  std::cout << "workspace.numel(): " << workspace.numel() << std::endl;
+
+  int64_t offset = 0;
+  auto workspace_data = workspace.data_ptr<unsigned char>();
+  mkldnn_workspace_arr.reserve(num_layers * num_directions);
+  for (const auto& desc : workspace_desc_arr) {
+    mkldnn_workspace_arr.emplace_back(
+        get_mkldnn_tensor((void*)(workspace_data + offset), desc));
+    offset += desc.get_size();
+  }
 }
 
 // MKLDNN RNN integration notes:
@@ -247,10 +309,10 @@ std::tuple<Tensor, Tensor, Tensor, Tensor> mkldnn_rnn(
     TORCH_CHECK(!cx_.defined(), "mkldnn_rnn: illegal defined cx for non-LSTM RNN");
   }
 
-  RNNParams fn(input_, batch_sizes, mode, hidden_size, num_layers, bidirectional, batch_first, train);
+  RNNParams rnn(input_, batch_sizes, mode, hidden_size, num_layers, bidirectional, batch_first, train);
 
   auto input = input_;
-  if (batch_first && !fn.is_input_packed()) {
+  if (batch_first && !rnn.is_input_packed()) {
     input = input.transpose(0, 1);
   }
   input = input.contiguous();
@@ -258,14 +320,18 @@ std::tuple<Tensor, Tensor, Tensor, Tensor> mkldnn_rnn(
   auto hx = hx_.contiguous();
   auto cx = cx_.defined() ? cx_.contiguous() : Tensor();
 
-  auto hy = at::empty(_hidden_size(fn), hx.options());
+  auto hy = at::empty(_hidden_size(rnn), hx.options());
   // NB: Not allowed to return undefined tensors
-  auto cy = cx.defined() ? at::empty(_hidden_size(fn), cx.options())
+  auto cy = cx.defined() ? at::empty(_hidden_size(rnn), cx.options())
                          : at::empty({0}, hx.options());
 
   MatrixRef<Tensor> weights{weight, static_cast<size_t>(weight_stride0)};
 
-  auto num_directions = fn.num_directions;
+  auto workspace = at::empty({0}, input.options().dtype(kByte));
+  std::vector<ideep::tensor> mkldnn_workspace_arr;
+  prepare_workspace(workspace, mkldnn_workspace_arr, rnn);
+
+  auto num_directions = rnn.num_directions;
   auto layer_input = input;
   for (int64_t layer = 0; layer < num_layers; layer++) {
     std::vector<Tensor> layer_output(num_directions);
@@ -276,19 +342,88 @@ std::tuple<Tensor, Tensor, Tensor, Tensor> mkldnn_rnn(
       auto layer_hy = hy[index];
       auto layer_cx = cx.defined() ? cx[index] : Tensor();
       auto layer_cy = cx.defined() ? cy[index] : at::empty({0}, input.options());
+      auto layer_ws = mkldnn_workspace_arr[index];
       auto reverse = (direction > 0);
-      layer_output[direction] = mkldnn_rnn_layer(layer_hy, layer_cy, layer_input, layer_weights, layer_hx, layer_cx, reverse, fn);
+      layer_output[direction] = mkldnn_rnn_layer(
+          layer_hy, layer_cy, layer_input, layer_weights, layer_hx, layer_cx, layer_ws, reverse, rnn);
     }
     layer_input = num_directions == 1 ? layer_output[0]
                                       : at::cat(layer_output, /*output_channels*/-1);
   }
   auto output = layer_input;
 
-  if (batch_first && !fn.is_input_packed()) {
+  if (batch_first && !rnn.is_input_packed()) {
     output.transpose_(0, 1);
   }
 
-  return std::make_tuple(output, hy, cy, Tensor());
+  return std::make_tuple(output, hy, cy, workspace);
+}
+
+std::tuple<Tensor, Tensor, Tensor, std::vector<Tensor>> mkldnn_rnn_backward(
+    const Tensor& input_, TensorList weight, int64_t weight_stride0,
+    const Tensor& hx_, const Tensor& cx_,
+    const Tensor& output_, const Tensor hy_, const Tensor cy_,
+    const Tensor& grad_output_, const Tensor& grad_hy_, const Tensor& grad_cy_,
+    int64_t mode, int64_t hidden_size,
+    int64_t num_layers, bool batch_first, double dropout_p,
+    bool train, bool bidirectional, IntArrayRef batch_sizes,
+    std::array<bool, 4> output_mask) {
+  TORCH_CHECK(train, "mkldnn_rnn_backward: can only be called in training mode");
+  TORCH_CHECK(dropout_p == 0.0, "mkldnn_rnn doesn't support dropout");
+  TORCH_CHECK(batch_sizes.size() == 0, "mkldnn_rnn doesn't support packed input");
+  if (static_cast<ideep::rnn_kind>(mode) != ideep::rnn_kind::LSTM) {
+    TORCH_CHECK(!cx_.defined(), "mkldnn_rnn_backward: illegal defined cx for non-LSTM RNN");
+  }
+  TORCH_CHECK(cx_.defined() || !output_mask[2], "illegally required grad of cx for non-LSTM RNN");
+
+  if (!grad_output_.defined() && !grad_hy_.defined() && !grad_cy_.defined()) {
+    return std::make_tuple(Tensor(), Tensor(), Tensor(), std::vector<Tensor>(weight.size()));
+  }
+
+  RNNParams rnn(input_, batch_sizes, mode, hidden_size, num_layers, bidirectional, batch_first, train);
+  auto input = input_;
+  auto output = output_;
+
+  auto memory_format = at::MemoryFormat::Contiguous;
+  auto grad_output = grad_output_.defined() ? grad_output_ : at::zeros_like(output, memory_format);
+  auto grad_hy = grad_hy_.defined() ? grad_hy_ : at::zeros_like(hx_, memory_format);
+  auto grad_cy = cx_.defined() ? (grad_cy_.defined() ? grad_cy_ : at::zeros_like(cx_, memory_format)) : grad_cy_;
+
+  if (batch_first && !rnn.is_input_packed()) {
+    input = input.transpose(0, 1);
+    output = output.transpose(0, 1);
+    grad_output = grad_output.transpose(0, 1);
+  }
+  input = input.contiguous();
+  output = output.contiguous();
+  grad_output = grad_output.contiguous();
+
+  auto hx = hx_.contiguous();
+  auto cx = cx_.defined() ? cx_.contiguous() : Tensor();
+  auto hy = hy_.contiguous();
+  auto cy = cy_.defined() ? cy_.contiguous() : Tensor();
+  grad_hy = grad_hy.contiguous();
+  grad_cy = grad_cy.contiguous();
+
+  auto grad_input = at::empty_like(input, memory_format);
+  auto grad_hx = at::empty_like(hx, memory_format);
+  auto grad_cx = cx_.defined() ? at::empty_like(cx, memory_format) : Tensor();
+
+  std::vector<Tensor> grad_weight;
+  grad_weight.reserve(weight.size());
+  for (const auto& w : weight) {
+    grad_weight.emplace_back(at::empty_like(w, memory_format));
+  }
+
+  MatrixRef<Tensor> weights{weight, static_cast<size_t>(weight_stride0)};
+  MatrixRef<Tensor> grad_weights{grad_weight, static_cast<size_t>(weight_stride0)};
+
+  std::cout << "mkldnn_rnn_backward" << std::endl;
+
+
+
+
+  return std::make_tuple(grad_input, grad_hx, grad_cx, grad_weight);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
